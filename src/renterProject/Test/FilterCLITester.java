663b61d6eaef6953df9e0bb43f3771d3e8d9c769
SamuelTJames <samuel.t.james145@gmail.com>
2026-07-23 17:09:58 -0500

    Update FilterCLITester.java

--- src/renterProject/Test/FilterCLITester.java
+++ src/renterProject/Test/FilterCLITester.java
@@ -11,14 +11,18 @@ class FilterCLITester {
 	@Test
 	void test() {
 		mainModel test = new mainModel();
-		String[][] testCases = {	{"filter","","","","","","","","","",""}, {"filter","","2","8","","","","","automatic","",""},
-								{"filter","","3","","electric","","","","automatic","",""}, {"filter","abc123","","","","","","","","",""}, 
-								{"filter","","3","5", "diesel", "volvo", "xc60", "2026", "automatic", "200", "2500"}};
+		String[][] testCases = {	{"BaseNothing","","","","","","","","","",""}, {"BasicFilter","","2","8","","","","","automatic","",""},
+								{"NoMatch","","3","","electric","","","","automatic","",""}, {"IDMatch","abc123","","","","","","","","",""}, 
+								{"OneMatch","","3","5","diesel","volvo","xc60","2026","automatic","200","2500"},
+								{"NumberError1","","notANumber","","","","","","","",""}, {"NumberError2","","9999999999","","","","","","","",""}};
 		assertEquals(test.database.length, (test.filterVehicles(testCases[0])).size());
 		assertEquals(3, (test.filterVehicles(testCases[1])).size());
 		assertEquals(0, (test.filterVehicles(testCases[2])).size());
 		assertEquals(1, (test.filterVehicles(testCases[3])).size());
 		assertEquals(1, (test.filterVehicles(testCases[4])).size());
+		
+		assertThrows(NumberFormatException.class, () -> { test.filterVehicles(testCases[5]).size(); });
+		assertThrows(NumberFormatException.class, () -> { test.filterVehicles(testCases[6]).size(); });
 	}
 
 }
